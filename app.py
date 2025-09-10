fec86f26131542bb8612f6896884685bf7b7779f
andreribeiro <prof.andrelbribeiro@gmail.com>
2025-09-09 22:07:50 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from utils.db import init_db
+from views import cadastro, conteudo, dashboard
+
+# Inicializa banco
+init_db()
+
+# Menu
+st.sidebar.title("📌 Menu")
+opcao = st.sidebar.radio("Navegação", ["Cadastro", "Conteúdo", "Dashboard","Documentos"])
+
+if opcao == "Cadastro":
+    cadastro.show()
+elif opcao == "Conteúdo":
+    conteudo.show()
+elif opcao == "Dashboard":
+    dashboard.show()
+elif opcao == "Documentos":
+    pass
---
+++ views/dashboard.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("📊 Dashboard Escolar")
+    st.write("Relatórios e estatísticas dos alunos aqui...")
